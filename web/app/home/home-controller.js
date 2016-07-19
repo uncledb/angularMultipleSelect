91afd38a1dd8d8f456c6be22d32408a172a06418
uncledb <434967007@qq.com>
2016-07-19 15:19:03 +0800

    v1.1.3 修改了下拉列表两个滚动条的bug

--- web/app/home/home-controller.js
+++ web/app/home/home-controller.js
@@ -1,48 +1,79 @@
 (function () {
 
-    angular.module('multipleSelect').controller('homeController', function ($scope) {
-        $scope.apiPath = "web/resources/skills.json";
+	angular.module('multipleSelect').controller('homeController', function ($scope) {
+		$scope.apiPath = "web/resources/skills.json";
 
-        $scope.skillsList = [
-            {id: 1, name : "Java"},
-            {id: 2, name : "C"},
-            {id: 3, name : "C++"},
-            {id: 4, name : "Core Java"},
-            {id: 5, name : "JavaScript"},
-            {id: 6, name : "PHP"},
-            {id: 7, name : "HTML"},
-            {id: 8, name : "CSS"},
-            {id: 9, name : "Angular Js"},
-            {id: 10, name : "Bootstrap"}
-        ];
+		$scope.skillsList = [
+			{id: 1, name: "Java"},
+			{id: 2, name: "C"},
+			{id: 3, name: "C++"},
+			{id: 4, name: "Core Java"},
+			{id: 5, name: "JavaScript"},
+			{id: 6, name: "PHP"},
+			{id: 7, name: "HTML"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 8, name: "CSS"},
+			{id: 8, name: "CSS"},
+			{id: 8, name: "CSS"},
+			{id: 8, name: "CSS"},
+			{id: 8, name: "CSS"},
+			{id: 8, name: "CSS"},
+			{id: 8, name: "CSS"},
+			{id: 8, name: "CSS"},
+			{id: 8, name: "CSS"},
+			{id: 8, name: "CSS"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js"},
+			{id: 9, name: "Angular Js111"}
 
-        $scope.skillsList1 = [
-            "Java",
-            "C",
-            "C++",
-            "Core Java",
-            "Javascript",
-            "PHP",
-            "MySql",
-            "Hibernate",
-            "Spring",
-            "AngularJs",
-            "BackboneJs",
-            "Sencha Touch",
-            "ExtJs"
-        ];
+		];
 
-        $scope.onSubmit = function () {
-            console.log("submit");
-            if($scope.multipleSelectForm.$invalid){
-                if($scope.multipleSelectForm.$error.required != null){
-                    $scope.multipleSelectForm.$error.required.forEach(function(element){
-                        element.$setDirty();
-                    });
-                }
-                return null;
-            }
-            alert("valid field");
-        };
-    });
+		$scope.skillsList1 = [
+			"Java",
+			"C",
+			"C++",
+			"Core Java",
+			"Javascript",
+			"PHP",
+			"MySql",
+			"Hibernate",
+			"Spring",
+			"AngularJs",
+			"BackboneJs",
+			"Sencha Touch",
+			"ExtJs"
+		];
+
+		$scope.onSubmit = function () {
+			console.log("submit");
+			if ($scope.multipleSelectForm.$invalid) {
+				if ($scope.multipleSelectForm.$error.required != null) {
+					$scope.multipleSelectForm.$error.required.forEach(function (element) {
+						element.$setDirty();
+					});
+				}
+				return null;
+			}
+			alert("valid field");
+		};
+	});
 })();
